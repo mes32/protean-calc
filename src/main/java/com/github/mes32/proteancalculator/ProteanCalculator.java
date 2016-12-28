898e97d8bf7bbe8f10e9c3727d1c0a37add4f3bb
mes32 <stockman.mike@gmail.com>
2016-12-28 01:33:04 -0500

    misc: Create project directory structure

---
+++ src/main/java/com/github/mes32/proteancalculator/ProteanCalculator.java
@@ -0,0 +1,10 @@
+
+//import java.io.*;
+
+package com.github.mes32.proteancalculator;
+
+class ProteanCalculator {
+	public static void main(String[] args) {
+		System.out.println("- Protean-Calculator -");
+	}
+}
