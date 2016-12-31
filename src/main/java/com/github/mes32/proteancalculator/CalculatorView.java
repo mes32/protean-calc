57cf3ef99d0f2d26f63c8b111debd95e1850668b
mes32 <stockman.mike@gmail.com>
2016-12-30 20:11:35 -0500

    feature: Add placeholder labels in place of buttons in CalculatorView

--- src/main/java/com/github/mes32/proteancalculator/CalculatorView.java
+++ src/main/java/com/github/mes32/proteancalculator/CalculatorView.java
@@ -8,6 +8,7 @@
 package com.github.mes32.proteancalculator;
 
 import java.awt.Dimension;
+import java.awt.Color;
 import java.io.*;
 import javax.swing.*;
 
@@ -22,8 +23,7 @@ class CalculatorView extends JFrame {
         this.viewModel = viewModel;
         this.controller = controller;
 
-        JPanel mainPanel = intiMainPanel();
-        add(mainPanel);
+        add(intiMainPanel());
 
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,21 +32,96 @@ class CalculatorView extends JFrame {
 
     private JPanel intiMainPanel() {
         JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.LINE_AXIS));
-        mainPanel.add(Box.createRigidArea(new Dimension(50, 0)));
+        mainPanel.add(Box.createRigidArea(viewModel.getCenterColumnLeftMargin()));
         mainPanel.add(initCenterColumn());
-        mainPanel.add(Box.createRigidArea(new Dimension(50, 0)));
+        mainPanel.add(Box.createRigidArea(viewModel.getCenterColumnRightMargin()));
 
         return mainPanel;
     }
 
     private JPanel initCenterColumn() {
         JPanel centerColumn = new JPanel();
+        centerColumn.setLayout(new BoxLayout(centerColumn, BoxLayout.Y_AXIS));
 
-        String labelText = "[Empty Label]";
-        JLabel label = new JLabel(labelText);
-        centerColumn.add(label);
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow1());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow2());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow3());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow4());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow5());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+        centerColumn.add(initRow6());
+        centerColumn.add(Box.createRigidArea(viewModel.getRowSpacer()));
+
         return centerColumn;
+    }
+
+    private JPanel initRow1() {
+        JPanel row1 = new JPanel();
+
+        String labelText = "[ DISPLAY ]";
+        JLabel label = new JLabel(labelText);
+        label.setBackground(new Color(180, 180, 255));
+        label.setForeground(Color.WHITE);
+        label.setOpaque(true);
+        row1.add(label);
+
+        return row1;
+    }
+
+    private JPanel initRow2() {
+        JPanel row2 = new JPanel();
+
+        String labelText = "[C] [\u00b1] [%] [\u00f7]";
+        JLabel label = new JLabel(labelText);
+        row2.add(label);
+
+        return row2;
+    }
+
+    private JPanel initRow3() {
+        JPanel row3 = new JPanel();
+
+        String labelText = "[7] [8] [9] [\u00d7]";
+        JLabel label = new JLabel(labelText);
+        row3.add(label);
+
+        return row3;
+    }
+
+        private JPanel initRow4() {
+        JPanel row4 = new JPanel();
+
+        String labelText = "[4] [5] [6] [-]";
+        JLabel label = new JLabel(labelText);
+        row4.add(label);
+
+        return row4;
+    }
+
+        private JPanel initRow5() {
+        JPanel row5 = new JPanel();
+
+        String labelText = "[1] [2] [3] [+]";
+        JLabel label = new JLabel(labelText);
+        row5.add(label);
+
+        return row5;
+    }
+
+        private JPanel initRow6() {
+        JPanel row6 = new JPanel();
+
+        String labelText = "[0   ] [.] [=]";
+        JLabel label = new JLabel(labelText);
+        row6.add(label);
+
+        return row6;
     }
 }
--- src/main/java/com/github/mes32/proteancalculator/CalculatorViewModel.java
+++ src/main/java/com/github/mes32/proteancalculator/CalculatorViewModel.java
@@ -7,11 +7,29 @@
 
 package com.github.mes32.proteancalculator;
 
+import java.awt.Dimension;
+
 class CalculatorViewModel {
 
     private CalculatorView view;
 
+    private static final Dimension CENTER_COLUMN_LEFT_MARGIN = new Dimension(20, 0);
+    private static final Dimension CENTER_COLUMN_RIGHT_MARGIN = new Dimension(20, 0);
+    private static final Dimension ROW_SPACER = new Dimension(0, 10);
+
     CalculatorViewModel(CalculatorView view) {
         this.view = view;
+    }
+
+    public Dimension getCenterColumnLeftMargin() {
+        return CENTER_COLUMN_LEFT_MARGIN;
+    }
+
+    public Dimension getCenterColumnRightMargin() {
+        return CENTER_COLUMN_RIGHT_MARGIN;
+    }
+
+    public Dimension getRowSpacer() {
+        return ROW_SPACER;
     }
 }
