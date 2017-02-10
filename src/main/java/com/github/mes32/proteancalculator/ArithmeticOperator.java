0e2225a3e824c83be62302655ce049cd7c2e336a
mes32 <stockman.mike@gmail.com>
2017-02-09 23:44:15 -0500

    minor: Fixed spelling of 'agregate' to 'aggregate'

--- src/main/java/com/github/mes32/proteancalculator/ArithmeticOperator.java
+++ src/main/java/com/github/mes32/proteancalculator/ArithmeticOperator.java
@@ -20,29 +20,29 @@ abstract class ArithmeticOperator {
     protected static final int DECIMAL_PRECISION = 9;
     protected static final MathContext MC = new MathContext(DECIMAL_PRECISION);
 
-    abstract public BigDecimal evaluate(BigDecimal agregate, BigDecimal actor);
+    abstract public BigDecimal evaluate(BigDecimal aggregate, BigDecimal actor);
 }
 
 class Add extends ArithmeticOperator {
-    public BigDecimal evaluate(BigDecimal agregate, BigDecimal actor) {
-        return agregate.add(actor, super.MC);
+    public BigDecimal evaluate(BigDecimal aggregate, BigDecimal actor) {
+        return aggregate.add(actor, super.MC);
     }
 }
 
 class Subtract extends ArithmeticOperator {
-    public BigDecimal evaluate(BigDecimal agregate, BigDecimal actor) {
-        return agregate.subtract(actor, super.MC);
+    public BigDecimal evaluate(BigDecimal aggregate, BigDecimal actor) {
+        return aggregate.subtract(actor, super.MC);
     }
 }
 
 class Divide extends ArithmeticOperator {
-    public BigDecimal evaluate(BigDecimal agregate, BigDecimal actor) {
-        return agregate.divide(actor, super.MC);
+    public BigDecimal evaluate(BigDecimal aggregate, BigDecimal actor) {
+        return aggregate.divide(actor, super.MC);
     }
 }
 
 class Multiply extends ArithmeticOperator {
-    public BigDecimal evaluate(BigDecimal agregate, BigDecimal actor) {
-        return agregate.multiply(actor, super.MC);
+    public BigDecimal evaluate(BigDecimal aggregate, BigDecimal actor) {
+        return aggregate.multiply(actor, super.MC);
     }
 }
